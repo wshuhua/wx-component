45896e715c6a2eecb9029ad3135661e159bde71c
wangshuhua <1730129114@qq.com>
2022-08-25 09:52:19 +0800

    feat: upgrade noticebar

---
+++ components/loading/index.js
@@ -0,0 +1,41 @@
+Component({
+    options: {
+        addGlobalClass: true
+    },
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        type: String,
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        loadingState: false,
+        maskState: false,
+        loadingText: "加载中..."
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        show({
+            mask = false,
+            title = "加载中..."
+        } = {}) {
+            this.setData({
+                loadingState: true,
+                maskState: mask,
+                loadingText: title
+            })
+        },
+        hide() {
+            this.setData({
+                loadingState: false
+            })
+        }
+    }
+})
